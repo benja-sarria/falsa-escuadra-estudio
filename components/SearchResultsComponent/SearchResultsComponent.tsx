3fa12cf0209dc1a00c60ea745d78892f9e0e7bea
benja-sarria <benja.sarria@live.com.ar>
2023-11-15 20:24:52 -0300

    feature | searchbox animation
    * Se implementó la animación del searchbox en desktop

--- components/SearchResultsComponent/SearchResultsComponent.tsx
+++ components/SearchResultsComponent/SearchResultsComponent.tsx
@@ -1,6 +1,8 @@
 "use client";
 import styles from "@/components/SearchResultsComponent/SearchResultsComponent.module.scss";
 import { useAppSelector } from "@/redux/store";
+import { SvgIcon } from "@mui/material";
+import GoldSearch from "@/public/assets/img/layout/goldsearch.svg";
 
 export const SearchResultsComponent = () => {
     const siteTexts = useAppSelector((state) => state.globalLanguage.value);
@@ -11,13 +13,21 @@ export const SearchResultsComponent = () => {
                 <h4 className={styles["title"]}>
                     {searchResultsTexts && searchResultsTexts.title}
                 </h4>
-                <input
-                    aria-label="search"
-                    type="text"
-                    name="search"
-                    id={"search"}
-                    className={styles["input"]}
-                />
+                <div className={styles["input-container"]}>
+                    <SvgIcon
+                        className={styles["gold-search"]}
+                        viewBox="100 100"
+                    >
+                        <GoldSearch />
+                    </SvgIcon>
+                    <input
+                        aria-label="search"
+                        type="text"
+                        name="search"
+                        id={"search"}
+                        className={styles["input"]}
+                    />
+                </div>
                 <ul className={styles["results-list"]}></ul>
             </div>
             <div></div>
