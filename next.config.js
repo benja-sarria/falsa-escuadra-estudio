4319e47d56b1397ac0fd0e61a5fd6fa76e2d7aaa
benja-sarria <benja.sarria@live.com.ar>
2024-04-13 18:58:44 -0300

    test | configure env for test

--- next.config.js
+++ next.config.js
@@ -11,11 +11,16 @@ const nextConfig = {
         serverActions: {
             allowedOrigins: [
                 "falsaescuadraestudio.com",
-                "localhost",
+                "localhost:4000",
+                "193.203.174.192",
                 "test.falsaescuadraestudio.com",
+            ],
+            allowedForwardedHosts: [
+                "localhost:4000",
+                "falsaescuadraestudio.com",
+                "test.falsaescuadraestudio.com",
+                "192.203.174.192",
             ],
         },
     },
 };
-
-module.exports = nextConfig;
